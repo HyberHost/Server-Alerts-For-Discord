ad6bc07f83e3e1115eb80715f95db7af18a382a1
Steven A. Smith <me@igsteven.com>
2022-11-28 00:22:05 +0000

    Create hh-testhook.sh

---
+++ scripts/hh-testhook.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script is provided with "No License" therefore using, modifying and the distribution of this script is not permitted without authorization from the rights holder(s).
+# © 2021-2022 Steven Smith (all rights reserved)
+
+source /etc/hh-config.sh
+
+client="${SSH_CLIENT%% *}"
+  /etc/stevescripts/discord.sh \
+  --webhook-url="$webhook_url" \
+  --username "$webhook_username" \
+  --avatar "$webhook_avatar" \
+  --description "Manual Test Alert!" \
+  --title "Manual Test Alert!" \
+  --field "Host;$server_name ($ip)" \
+  --author "@Steven#7194's Login Alerts" \
+  --author-url "https://github.com/IGSteven" \
+  --author-icon "https://avatars.githubusercontent.com/u/22038054" \
+  --footer "$webhook_footer"
